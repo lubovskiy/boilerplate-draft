cfb4876435094db49a9d476daae9d5b0f9a4c8f0
Andrey Lubovskiy <andlubovskiy@gmail.com>
2021-06-15 12:06:19 +0300

    service struct

--- internal/service/echo.go
+++ internal/service/echo.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"context"
+
+	service "github.com/lubovskiy/test-service/pkg"
+	"github.com/sirupsen/logrus"
+)
+
+type TestService struct {
+	Logger *logrus.Logger
+}
+
+func NewTestService() *TestService {
+	return &TestService{
+		Logger: logrus.New(),
+	}
+}
+
+func (s *TestService) Echo(ctx context.Context, in *service.EchoRequest) (*service.EchoResponse, error) {
+	return &service.EchoResponse{
+		Out: in.In,
+	}, nil
+}
